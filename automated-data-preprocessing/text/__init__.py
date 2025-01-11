e9ce2c5201174120ee05d84db93ec41559d14ae1
Haricrim <habibhbn3@gmail.com>
2025-01-11 20:02:36 +0100

    ...

--- automated-data-preprocessing/text/__init__.py
+++
@@ -1,9 +0,0 @@
-description = "The make_clean command line utility program automatically " \
-              "performs common data preprocessing tasks on your uncleaned data sets.\n"
-
-options = {'-a': '-c, -i, -o',
-           '-c': 'file contains categorical data',
-           '-i': 'interpolate missing values',
-           '-m': 'file is missing field names',
-           '-o': 'outlier detection and removal'}
-
